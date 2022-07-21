16a4f94ad69f37aee4a882aae223ee5b34e433c5
sherinskollannur <sherinskollannur2015@gmail.com>
2022-07-21 09:47:24 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Its a covid 19 tracker
         </a>
       </header>
     </div>
